f61d06e0c774065def9c843cd25866bc7377a3a6
IlyaGershman <ilgershman@gmail.com>
2022-04-24 15:13:25 +0300

    editable table - start

--- components/ui-components/EditableTable.js
+++ components/ui-components/EditableTable.js
@@ -1,134 +1,51 @@
-import React from "react";
-import { Table, Input, InputNumber } from "antd";
-import { data } from "autoprefixer";
+import React, {useState} from "react";
+import { Table, Input, InputNumber, Popconfirm, Typography } from "antd";
 
-const EditableContext = React.createContext();
+const mapData = (data) => data.map(dataRow => ({key: dataRow._id, ...dataRow}))
+// const mapColumns = (fields) => fields.map(field => {
+//   field
 
-class EditableCell extends React.Component {
-  getInput = () => {
-    if (this.props.inputType === "number") {
-      return <InputNumber />;
+export default ({data, fields}) => {
+  console.log(data);
+
+  const [editingKey, setEditingKey] = useState('');
+  const isEditing = (row) => row.key === editingKey;
+  
+  const dataSource = mapData(data);
+  
+  const edit = (record) => {
+    setEditingKey(record.key);
+  };
+  
+  const cancel = () => {
+    setEditingKey('');
+  };
+
+  const actionsColumn = {
+    "title": "Actions",
+    "dataIndex": "action",
+    "width": "50%",
+    render: (_, record) => {
+      const editable = isEditing(record);
+      return editable ? (
+        <span>
+          <Typography.Link onClick={() => save(record.key)} style={{ marginRight: 8 }}>
+            Save
+          </Typography.Link>
+          <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
+            <a>Cancel</a>
+          </Popconfirm>
+        </span>
+      ) : (
+        <Typography.Link disabled={editingKey !== ''} onClick={() => edit(record)}>
+          Edit
+        </Typography.Link>
+      );
     }
-    return <Input />;
-  };
-
-  renderCell = () => {
-    const {
-      editing,
-      dataIndex,
-      title,
-      inputType,
-      record,
-      index,
-      children,
-      ...restProps
-    } = this.props;
-    return <td {...restProps}>{editing ? "Editing" : children}</td>;
-  };
-
-  render() {
-    return (
-      <EditableContext.Consumer>{this.renderCell}</EditableContext.Consumer>
-    );
-  }
-}
-
-class EditableTable extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { data: props.data, editingKey: "" };
-    this.columns = [
-      ...props.fields,
-      /*  {
-        title: "operation",
-        dataIndex: "operation",
-        render: (text, record) => {
-          const { editingKey } = this.state;
-          const editable = this.isEditing(record);
-          return editable ? (
-            <span>me is editing</span>
-          ) : (
-            <a
-              disabled={editingKey !== ""}
-              onClick={() => this.edit(record.key)}
-            >
-              Edit
-            </a>
-          );
-        },
-      }, */
-    ];
   }
 
-  isEditing = (record) => record.key === this.state.editingKey;
+  const columns = [...fields, actionsColumn];
+  
+  return <Table dataSource={dataSource} columns={columns} />;
+};
 
-  cancel = () => {
-    this.setState({ editingKey: "" });
-  };
-
-  save(form, key) {
-    form.validateFields((error, row) => {
-      if (error) {
-        return;
-      }
-      const newData = [...this.state.data];
-      const index = newData.findIndex((item) => key === item.key);
-      if (index > -1) {
-        const item = newData[index];
-        newData.splice(index, 1, {
-          ...item,
-          ...row,
-        });
-        this.setState({ data: newData, editingKey: "" });
-      } else {
-        newData.push(row);
-        this.setState({ data: newData, editingKey: "" });
-      }
-    });
-  }
-
-  edit(key) {
-    this.setState({ editingKey: key });
-  }
-
-  render() {
-    const components = {
-      body: {
-        cell: EditableCell,
-      },
-    };
-
-    const columns = this.columns.map((col) => {
-      if (!col.editable) {
-        return col;
-      }
-      return {
-        ...col,
-        onCell: (record) => ({
-          record,
-          inputType: col.dataIndex === "age" ? "number" : "text",
-          dataIndex: col.dataIndex,
-          title: col.title,
-          editing: this.isEditing(record),
-        }),
-      };
-    });
-
-    return (
-      <EditableContext.Provider value={this.props.form}>
-        <Table
-          components={components}
-          bordered
-          dataSource={this.state.data}
-          columns={columns}
-          rowClassName="editable-row"
-          pagination={{
-            onChange: this.cancel,
-          }}
-        />
-      </EditableContext.Provider>
-    );
-  }
-}
-
-export default EditableTable;
