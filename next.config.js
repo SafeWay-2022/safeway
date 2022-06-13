e6446384f586e206434fed36513c57e291e3f99a
Alexey <paet14@gmail.com>
2022-06-13 21:55:42 +0300

    changed config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_REMOTE_HOST}/:path`,
+      },
+    ]
+  },
   images: {
     loader: "imgix",
     path: "/",
