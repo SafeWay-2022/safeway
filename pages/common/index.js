957bfe89c208901649bf40130ba23b58f91519ca
Alexey <paet14@gmail.com>
2022-07-23 01:27:10 +0300

    added common table

--- pages/common/index.js
+++ pages/common/index.js
@@ -1,15 +1,155 @@
-import PageTable from '../../components/PageTable';
+import { useState } from 'react';
+import { Pagination, Skeleton, Button, Radio, Table, Checkbox, Tag, Popconfirm } from 'antd';
+import {
+    RollbackOutlined,
+    SearchOutlined,
+    DeleteOutlined,
+    DownOutlined,
+    UpOutlined,
+} from '@ant-design/icons';
+import { useQuery } from 'react-query';
+import { PER_PAGE } from '../../config';
+import { nanoid } from 'nanoid';
+import { updatePoint, createPoint, getTableFetch, initialPoint, deletePoint } from '../../lib/helpers';
 import styles from '../../styles/Home.module.css';
 
-export default function DynamicPage({ currentTable, commonTables }) {
-    if (!currentTable) {
-        return <h1>Error: no table...</h1>;
+
+
+export default function PageTable() {
+    const [tableConfig, setTableConfig] = useState({
+        route: '/common/'
+    })
+    const { route } = tableConfig;
+    const myFetch = getTableFetch(route)
+    const [page, setPage] = useState(0);
+    const [limit, setLimit] = useState(PER_PAGE);
+    const [searchData, setSearchData] = useState({})
+    const [mapView, setMapView] = useState(false)
+    const [value, setValue] = useState({})
+
+    const {
+        data: tableData,
+        error,
+        isLoading,
+        isFetching,
+        refetch,
+    } = useQuery([route, page, limit], () => myFetch(
+        {
+            skip: page,
+            limit: limit,
+            ...searchData
+        }), {
+        refetchOnWindowFocus: true,
+        keepPreviousData: true,
+        staleTime: 15000,
+        refetchInterval: 0,
+    });
+
+
+    const columns = [
+        {
+            title: "Category",
+            dataIndex: "category",
+            render: (name) => {
+                return (
+                    <Tag style={{ fontSize: '14px' }}>{name}</Tag>
+                )
+            }
+        },
+        {
+            title: "Українська",
+            dataIndex: "ua",
+            render: (ukr) => {
+                return <span>{ukr}</span>
+            }
+        },
+        {
+            title: "Русский",
+            dataIndex: "ru",
+            render: (ru) => {
+                return <span>{ru}</span>
+            }
+        },
+        {
+            title: "Description",
+            dataIndex: "description",
+            render: (description) => {
+                return <span>{description}</span>
+            }
+        },
+        {
+            title: "Icon",
+            dataIndex: "icon",
+            render: (icon) => {
+                return (
+                    <>
+                        {icon && <img src={icon} alt="icon" width="50px" height="50px" />}
+                    </>
+                )
+            }
+        },
+        {
+            title: "Action",
+            dataIndex: "",
+            key: "x",
+            render: (record) => {
+                return (
+                    <div style={{ display: 'flex' }}>
+                        {/* <Modal isTable={true} record={record} refetch={refetch} doFetch={updatePoint} title="Edit point" /> */}
+                        <Popconfirm
+                            placement="top"
+                            title="Do you really want to delete this item?"
+                            onConfirm={() => deletePoint(record._id, refetch)}
+                            okText="Delete"
+                            okType="secondary"
+                            cancelText="Cancel">
+                            <DeleteOutlined style={{ fontSize: '150%', cursor: 'pointer' }} />
+                        </Popconfirm>
+                    </div>
+                )
+            }
+        },
+    ]
+
+
+
+
+    if (error) {
+        return <h1>Error getting table data:{JSON.stringify(error)}</h1>;
     }
+
+    if (isLoading) {
+        return <Skeleton />;
+    }
+    const { list = [], skip = 0, total = 0 } = tableData;
+    const pagination = {
+        pageSize: limit,
+        onChange: (currentPage, limit) => {
+            if (currentPage === 1) {
+                setPage(0);
+            } else {
+                setPage((currentPage - 1) * limit);
+            }
+            setLimit(limit);
+        },
+        total,
+    };
+
+
     return (
         <div className={styles.container}>
             <main>
-                <PageTable table={currentTable} commonTables={commonTables} />
-            </main>
-        </div>
+
+                {total > 0 && <Pagination style={{ display: 'inline' }} {...pagination} />}
+
+                <Table
+                    loading={isFetching}
+                    columns={columns}
+                    dataSource={tableData?.list}
+                    pagination={false}
+                />
+
+            </main >
+        </div >
     );
 }
--- pages/need/index.js
+++ pages/need/index.js
@@ -1,15 +1,185 @@
-import PageTable from '../../components/PageTable';
+import { useState } from 'react';
+import { Pagination, Skeleton, Button, Radio, Table, Checkbox, Tag, Popconfirm } from 'antd';
+import {
+    RollbackOutlined,
+    SearchOutlined,
+    DeleteOutlined,
+    DownOutlined,
+    UpOutlined,
+} from '@ant-design/icons';
+import { useQuery } from 'react-query';
+import { PER_PAGE } from '../../config';
+import { nanoid } from 'nanoid';
+import { updatePoint, createPoint, getTableFetch, initialPoint, deletePoint } from '../../lib/helpers';
 import styles from '../../styles/Home.module.css';
 
-export default function DynamicPage({ currentTable, commonTables }) {
-    if (!currentTable) {
-        return <h1>Error: no table...</h1>;
+
+
+export default function PageTable() {
+    const [tableConfig, setTableConfig] = useState({
+        route: '/users/'
+    })
+    const { route } = tableConfig;
+    const myFetch = getTableFetch(route)
+    const [page, setPage] = useState(0);
+    const [limit, setLimit] = useState(PER_PAGE);
+    const [searchData, setSearchData] = useState({})
+    const [mapView, setMapView] = useState(false)
+    const [value, setValue] = useState({})
+
+    const {
+        data: tableData,
+        error,
+        isLoading,
+        isFetching,
+        refetch,
+    } = useQuery([route, page, limit], () => myFetch(
+        {
+            skip: page,
+            limit: limit,
+            ...searchData
+        }), {
+        refetchOnWindowFocus: true,
+        keepPreviousData: true,
+        staleTime: 15000,
+        refetchInterval: 0,
+    });
+
+    const columns = [
+        {
+            title: "Name",
+            dataIndex: "full_name",
+            render: (name) => {
+                return (
+                    <Tag style={{ fontSize: '14px' }}>{name}</Tag>
+                )
+            }
+        },
+        {
+            title: "Phone",
+            dataIndex: "phone",
+            render: (Phone) => {
+                return <span>{Phone}</span>
+            }
+        },
+        {
+            title: "Email",
+            dataIndex: "email",
+            render: (site) => {
+                return <span>{site}</span>
+            }
+        },
+        {
+            title: "Managed cities",
+            dataIndex: "managed_cities",
+            render: (cities) => {
+                return (
+                    <>
+                        {cities?.map(e => (<Tag key={nanoid()} color="green">{e}</Tag>))}
+                    </>
+                )
+            }
+        },
+        {
+            title: "Managed countries",
+            dataIndex: "managed_countries",
+            render: (cities) => {
+                return (
+                    <>
+                        {cities?.map(e => (<Tag key={nanoid()} color="green">{e}</Tag>))}
+                    </>
+                )
+            }
+        },
+        {
+            title: "Organization position",
+            dataIndex: "organization_position",
+            render: (position) => {
+                return (
+                    <span>{position}</span>
+                )
+            }
+        },
+        {
+            title: "Organization membership",
+            dataIndex: "organization_membership",
+            render: (position) => {
+                return (
+                    <span>{position}</span>
+                )
+            }
+        },
+        {
+            title: "Super user",
+            dataIndex: "superuser",
+            render: (position) => {
+                return (
+                    <span>{position}</span>
+                )
+            }
+        },
+        {
+            title: "Action",
+            dataIndex: "",
+            key: "x",
+            render: (record) => {
+                return (
+                    <div style={{ display: 'flex' }}>
+                        {/* <Modal isTable={true} record={record} refetch={refetch} doFetch={updatePoint} title="Edit point" /> */}
+                        <Popconfirm
+                            placement="top"
+                            title="Do you really want to delete this item?"
+                            onConfirm={() => deletePoint(record._id, refetch)}
+                            okText="Delete"
+                            okType="secondary"
+                            cancelText="Cancel">
+                            <DeleteOutlined style={{ fontSize: '150%', cursor: 'pointer' }} />
+                        </Popconfirm>
+                    </div>
+                )
+            }
+        },
+    ]
+
+
+
+
+    if (error) {
+        return <h1>Error getting table data:{JSON.stringify(error)}</h1>;
     }
+
+    if (isLoading) {
+        return <Skeleton />;
+    }
+    const { list = [], skip = 0, total = 0 } = tableData;
+    const pagination = {
+        pageSize: limit,
+        onChange: (currentPage, limit) => {
+            if (currentPage === 1) {
+                setPage(0);
+            } else {
+                setPage((currentPage - 1) * limit);
+            }
+            setLimit(limit);
+        },
+        total,
+    };
+
+
     return (
         <div className={styles.container}>
             <main>
-                <PageTable table={currentTable} commonTables={commonTables} />
-            </main>
-        </div>
+
+                {total > 0 && <Pagination style={{ display: 'inline' }} {...pagination} />}
+
+                <Table
+                    loading={isFetching}
+                    columns={columns}
+                    dataSource={tableData?.list}
+                    pagination={false}
+                />
+
+            </main >
+        </div >
     );
 }
